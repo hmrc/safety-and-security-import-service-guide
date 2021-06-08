ff8200c0a299ce9cd555cceeda3e5fd879e27a8a
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2021-06-08 16:48:04 +0100

    DL-5315 upgrade bootstrap to play 27

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import play.sbt.PlayImport._
 import sbt._
 
 object AppDependencies {
-  val bootStrapPlayVersion = "1.16.0"
+  val bootStrapPlayVersion = "5.2.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-26" % bootStrapPlayVersion)
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % bootStrapPlayVersion)
 
   val test: Seq[ModuleID] = Seq(
     "org.pegdown" % "pegdown" % "1.6.0" % "test",
-    "org.scalatest" %% "scalatest" % "3.0.9" % "test")
+    "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3" % "test")
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -7,4 +7,4 @@ addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.0.0")
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.1.0")
 
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.6.24")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.7.9")
